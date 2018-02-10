4ac843caed6559be573273bf6bb18ec145c1aedc
dangguz <32621493+dangguz@users.noreply.github.com>
2018-02-10 12:19:03 +0100

    Add new contract

---
+++ contracts/OneUserOneOwner.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.8;
+
+import "./lib/Owned.sol";
+
+contract OneUserOneOwner is Owned {
+    address user;
+
+    modifier onlyUser() {
+        require (msg.sender == user);
+        _;
+    }
+
+    function OneUserOneOwner() {}
+
+    function transferUser (address newUser) public onlyOwner {
+        if (newUser != user) {
+            user = newUser;
+        }
+    }
+
+    // Getter function of the user address
+    function getUser () public onlyOwner returns (address user) {
+        return user;
+    }
+}
